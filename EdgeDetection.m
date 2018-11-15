da6426159aa106ea8c34304d792ffa28c9684d8c
Thomak Leduc <tleduc001@ensc.fr>
2018-11-15 17:45:30 +0100

    corner tracking in progress

--- EdgeDetection.m
+++ EdgeDetection.m
@@ -3,6 +3,7 @@ clear all
 
 v2= VideoReader('template.mp4');
 
+
 image = read(v2,1);
 imshow(image);
 [x,y] = ginput(4);
@@ -12,10 +13,16 @@ x(5) = x(1);
 y(5)= y(1);
 plot(x,y);
 
-figure,
+% Save the coordinates of each corner into the variable coordinatesCorner
+% (manually)
+coordinatesCorner = [x(1),y(1),x(2),y(2),x(3),y(3),x(4),y(4)];
+predictPoints = ones(8);
 
 
-for n = [1:20]
+% figure,
+
+
+for n = [1:5]
     im=read(v2,n);
     imD=double(im);
 
@@ -29,37 +36,64 @@ for n = [1:20]
 
 
     %imshow(G, [0 400]);
-    colormap(flipud(gray(256)));
+%     colormap(flipud(gray(256)));
         
     %[x,y]=cornerPage(G);
 
     D = harris(G,0.05, Ix, Iy,2).*abs(harris(G,0.05, Ix, Iy, 5));
     %D=max(D,0);
     %colormap(flipud(gray(256)));
-    imshow(D, [0 10]);
-        
+%     imshow(D, [0 10]);
     
-    F(n)=getframe(gcf);
-    drawnow
+    % find the best corner for each 4 corners of the page
+    if n == 1
+        [ xA,yA ] = CornerAnalysis(D,x(1),y(1),40 );
+        [ xB,yB ] = CornerAnalysis(D,x(2),y(2),40);
+        [ xC,yC ] = CornerAnalysis(D,x(3),y(3),40);
+        [ xD,yD ] = CornerAnalysis(D,x(4),y(4),40);
+   
+    else
+
+        [ xA,yA ] = CornerAnalysis(D,coordinatesCorner(n,1),coordinatesCorner(n,2),40);
+        [ xB,yB ] = CornerAnalysis(D,coordinatesCorner(n,3),coordinatesCorner(n,4),40);
+        [ xC,yC ] = CornerAnalysis(D,coordinatesCorner(n,5),coordinatesCorner(n,6),40);
+        [ xD,yD ] = CornerAnalysis(D,coordinatesCorner(n,7),coordinatesCorner(n,8),40);
+
+    end
+    
+
+
+    
+    % Save the new coordinates of each corner into the variable coordinatesCorner
+    coordinatesCorner = [coordinatesCorner;xA,yA,xB,yB,xC,yC,xD,yD];
+    
+    % Predict the new coordinates of each corner into the variable coordinatesCorner   
+    if n > 3
+        predictPoints =  predictCorners(predictPoints,coordinatesCorner,n);
+    end
+    
+    
+    
+%     hold on
+%     scatter(x,y);
+%     plot(x,y);
+    
+    
+    
+%     
+   
+    
+%     F(n)=getframe(gcf);
+%     drawnow
+
+% predictPoints
 end
+
+% Display the variables
+% coordinatesCorner
+% predictPoints
 
 %------------ Ecrire la video -----------------------
-
-% create the video writer with 1 fps
-writerObj = VideoWriter('myVideo.mp4', 'MPEG-4');
-writerObj.FrameRate = 2;
-% set the seconds per image
-
-% open the video writer
-open(writerObj);
-
-% write the frames to the video
-for i=1:length(F)
-    % convert the image to a frame
-    frame = F(i) ;    
-    writeVideo(writerObj, frame);
-end
-% close the writer object
-close(writerObj);
+% WriteVideoOutput(F);
 
 
